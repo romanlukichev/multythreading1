f63811d864c524bcc2e51a38cbb7437b6a556883
romanlukichev <roman.lukichev@yahoo.com>
2017-11-14 17:38:12 -0800

    the third way to run mutythreading

--- src/com/jobready/threading/Application.java
+++ src/com/jobready/threading/Application.java
@@ -7,14 +7,39 @@ public class Application {
 
         System.out.println("Starting thread B");
         Task taskRunner1 = new Task("thread-B");
-        Thread t1 = new Thread(taskRunner1);
-        // or Thread t1 = new Thread(new Task("thread-B"));
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //Thread.currentThread().setName(this.name);
+                // because ther is no name property in annonymous class
+                for( int i=0 ; i < 1000 ; i++ ){
+                    System.out.println( "number: " + i + " " + Thread.currentThread().getName() );
+                    try{
+                        Thread.sleep(10);
+                    } catch (InterruptedException e){
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
         t1.start();
 
         System.out.println("Starting thread A");
         Task taskRunner2 = new Task("thread-A");
-        Thread t2 = new Thread(taskRunner2);
-        // or Thread t2 = new Thread(new Task("thread-A"));
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //Thread.currentThread().setName(this.name);
+                for( int i=0 ; i < 1000 ; i++ ){
+                    System.out.println( "number: " + i + " " + Thread.currentThread().getName() );
+                    try{
+                        Thread.sleep(10);
+                    } catch (InterruptedException e){
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
         t2.start();
 
 
