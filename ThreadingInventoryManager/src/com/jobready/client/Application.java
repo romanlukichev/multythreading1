de7a1f5608ac53abf3fa366e0e59d952ad7f84c3
romanlukichev <roman.lukichev@yahoo.com>
2017-11-20 14:36:47 -0800

    Thread safety with collection - contains an error

---
+++ ThreadingInventoryManager/src/com/jobready/client/Application.java
@@ -0,0 +1,33 @@
+package com.jobready.client;
+import com.jobready.inventory.InventoryManager;
+
+/**
+ * Created by Roman on 16.11.2017.
+ */
+public class Application {
+
+    public static void main(String[] args) throws InterruptedException {
+
+    InventoryManager manager;
+        manager = new InventoryManager();
+
+        Thread inventoryTask = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            manager.populateSoldProducts();
+        }
+    });
+
+    Thread displayTask = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            manager.displaySoldProducts();
+        }
+    });
+
+    inventoryTask.start();
+            Thread.sleep(2000);
+        displayTask.start();
+
+    }
+}
